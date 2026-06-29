f0187166ca6da23d424f96e62d87e444b02db4d5
jaaaa8 <bileyghas1eye@gmail.com>
2026-06-29 16:49:05 +0700

    Revert "Vanhieu done sprint 1"

--- src/main/java/com/example/m6_thermal_power_plant_api/service/expertise/IExpertiseService.java
+++
@@ -1,8 +0,0 @@
-package com.example.m6_thermal_power_plant_api.service.expertise;
-
-import com.example.m6_thermal_power_plant_api.dto.employee.ExpertiseDTO;
-import java.util.List;
-
-public interface IExpertiseService {
-    List<ExpertiseDTO> getAllExpertises();
-}
