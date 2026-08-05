5e9cce486814c6a45719f4500789fffc37e046b5
phonghao122 <phonghao122@gmail.com>
2026-08-05 20:42:09 +0700

    create lubrication History

--- src/main/java/com/example/m6_thermal_power_plant_api/service/leader/lubrication_plan/LubricationPlanService.java
+++ src/main/java/com/example/m6_thermal_power_plant_api/service/leader/lubrication_plan/LubricationPlanService.java
@@ -280,8 +280,6 @@ public class LubricationPlanService implements ILubricationPlanService {
 
     @Scheduled(cron = "0 0 0 * * ?")
     public void autoUpdateLubricationStatus(){
-
         loadStatus();
-
     }
 }
