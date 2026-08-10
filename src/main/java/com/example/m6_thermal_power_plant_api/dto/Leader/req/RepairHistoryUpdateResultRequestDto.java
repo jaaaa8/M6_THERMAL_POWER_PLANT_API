bf91142bf7ff2b56b66f676287f094decae36f46
jaaaa8 <bileyghas1eye@gmail.com>
2026-08-10 19:57:10 +0700

    feat: allow updating repair history result

---
+++ src/main/java/com/example/m6_thermal_power_plant_api/dto/Leader/req/RepairHistoryUpdateResultRequestDto.java
@@ -0,0 +1,10 @@
+package com.example.m6_thermal_power_plant_api.dto.Leader.req;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RepairHistoryUpdateResultRequestDto {
+    private String repairResult;
+}
--- src/main/java/com/example/m6_thermal_power_plant_api/service/leader/repair_history/IRepairHistoryService.java
+++ src/main/java/com/example/m6_thermal_power_plant_api/service/leader/repair_history/IRepairHistoryService.java
@@ -1,6 +1,7 @@
 package com.example.m6_thermal_power_plant_api.service.leader.repair_history;
 
 import com.example.m6_thermal_power_plant_api.dto.Leader.req.RepairHistoryCreateRequestDto;
+import com.example.m6_thermal_power_plant_api.dto.Leader.req.RepairHistoryUpdateResultRequestDto;
 import com.example.m6_thermal_power_plant_api.dto.Leader.res.RepairHistoryResponseDto;
 import com.example.m6_thermal_power_plant_api.entity.WorkOrder;
 
@@ -14,6 +15,11 @@ public interface IRepairHistoryService {
     );
 
     RepairHistoryResponseDto findById(Integer id);
+
+    RepairHistoryResponseDto updateResult(
+            Integer id,
+            RepairHistoryUpdateResultRequestDto dto
+    );
 
     void createRepairHistory(WorkOrder workOrder);
     List<RepairHistoryResponseDto> findByEquipment(Integer equipmentId);
